16e5269b028d23ad815d4a1bf8c79b5a4c55f07b
mcostamgna <marcelo.costamagna@fligoo.com>
2022-03-24 22:08:57 -0300

    adding new fields to testfligoo tables

--- sql/init.sql
+++ sql/init.sql
@@ -2,4 +2,4 @@ CREATE USER fligoo WITH PASSWORD 'fligoo';
 CREATE DATABASE testfligoo owner fligoo;
 GRANT ALL PRIVILEGES ON DATABASE "testfligoo" to fligoo;
 \connect testfligoo;
-CREATE TABLE IF NOT EXISTS testdata (flight_date VARCHAR (50) NOT NULL, flight_status VARCHAR (50) NOT NULL);
+CREATE TABLE IF NOT EXISTS testdata (flight_date VARCHAR (50), flight_status VARCHAR (50), departure_airport VARCHAR (50), departure_timezone VARCHAR (50), arrival_airport VARCHAR (50), arrival_timezone VARCHAR (50), arrival_terminal VARCHAR (50), airline_name VARCHAR (50), flight_number VARCHAR (50) );
