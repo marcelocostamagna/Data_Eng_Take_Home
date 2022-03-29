4e927cb114c8e863edd76aa08d0501549569f5b1
mcostamgna <marcelo.costamagna@fligoo.com>
2022-03-29 00:22:45 -0300

    modify table columns lenght to 200

--- sql/init.sql
+++ sql/init.sql
@@ -2,4 +2,4 @@ CREATE USER fligoo WITH PASSWORD 'fligoo';
 CREATE DATABASE testfligoo owner fligoo;
 GRANT ALL PRIVILEGES ON DATABASE "testfligoo" to fligoo;
 \connect testfligoo;
-CREATE TABLE IF NOT EXISTS testdata (flight_date VARCHAR (50), flight_status VARCHAR (50), departure_airport VARCHAR (50), departure_timezone VARCHAR (50), arrival_airport VARCHAR (50), arrival_timezone VARCHAR (50), arrival_terminal VARCHAR (50), airline_name VARCHAR (50), flight_number VARCHAR (50) );
+CREATE TABLE IF NOT EXISTS testdata (flight_date VARCHAR (200), flight_status VARCHAR (200), departure_airport VARCHAR (200), departure_timezone VARCHAR (200), arrival_airport VARCHAR (200), arrival_timezone VARCHAR (200), arrival_terminal VARCHAR (200), airline_name VARCHAR (200), flight_number VARCHAR (200) );
